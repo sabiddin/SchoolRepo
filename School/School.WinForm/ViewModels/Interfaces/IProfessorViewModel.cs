0069fefd947b68486e95c0d4a00d1f473c507552
sabiddin <abiddinsyed@gmail.com>
2018-03-01 18:49:43 -0500

    master: Added professor module.

---
+++ School/School.WinForm/ViewModels/Interfaces/IProfessorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School.WinForm.ViewModels.Interfaces
+{
+    public interface IProfessorViewModel
+    {
+    }
+}
